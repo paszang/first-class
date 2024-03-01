8a4f762d5295725b4ec46c89a0709afa7431f210
unknown <02230268.cst@rub.edu.bt>
2024-03-01 22:59:25 +0530

    logical.py

--- logical.py
+++ logical.py
@@ -1,10 +1,28 @@
-x = int(input("Enter your age:"))
-y= str(input("Are you a student? (Yes/No): "))
+# Eaxample 1
+x = 5
+print(x > 3 and x < 10) # True because both the conditions are True 
 
-if  (x<=12 or (x>=13 or x<=18)) and y=="Yes":
-    print("You are eligible for discount on ticket.")
-else:
-    print("You are not eligible  for a discount on the moive ticket.")
+# Example 2
+y = 12
+print(y > 10 and y % 5 ==0) #False because the second condition is False
+
+# Example 3
+z = 5
+print(z < 3 or z > 10) # False because both the conditions are False
+
+# Example 4
+w = 12
+print (w < 10 or w % 2 == 0) #True because the second condition is True 
+
+# Example 5
+v = 5
+print(not(v > 3 and v < 10)) #False because the condition inside the not is True
+
+# Example 6
+u = 12
+print(not(u > 10 and u % 5 ==0)) #True because the condition inside the not is False
+
+
 
  
  
