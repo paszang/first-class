387c5381e174e2a9cdf92424d819f7cc69013220
unknown <02230268.cst@rub.edu.bt>
2024-03-01 14:22:28 +0530

    change to logical.py

---
+++ logical.py
@@ -0,0 +1,10 @@
+x = int(input("Enter your age:"))
+y= str(input("Are you a student? (Yes/No): "))
+
+if  (x<=12 or (x>=13 or x<=18)) and y=="Yes":
+    print("You are eligible for discount on ticket.")
+else:
+    print("You are not eligible  for a discount on the moive ticket.")
+
+ 
+ 
