6f622014a6d8c6f4aa1f0dc57f9721ae0616b06a
unknown <02230268.cst@rub.edu.bt>
2024-03-01 23:36:47 +0530

    discount.py

---
+++ discount.py
@@ -0,0 +1,11 @@
+x = int(input("Enter your age:"))
+y = str(input("Are you a student? (yes/no):")).lower()
+
+if (x<=12 or( x>=13 or x<=18)) and y =="yes":
+    print("You are eligilbe for a discount on the movie ticket.")
+
+else:
+    print("You are not eligible for a discount on the movie ticket.")
+
+
+
