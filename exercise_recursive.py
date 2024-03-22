dcb4fb517cc93aeab517122cb9efd0d0e3112aa5
unknown <02230268.cst@rub.edu.bt>
2024-03-22 14:06:25 +0530

    file safe

---
+++ exercise_recursive.py
@@ -0,0 +1,13 @@
+def reverse_string(s):
+    # Base case: if the string is empty or has only one character, return the string itself
+    if len(s) <= 1:
+        return s
+    # Recursive case: concatenate the last character with the reversed substring of the string excluding the last character
+    else:
+        return s[-1] + reverse_string(s[:-1])
+
+# Example usage:
+input_string = "Tupac , Shakur!"
+reversed_string = reverse_string(input_string)
+print("Original string:", input_string)
+print("Reversed string:", reversed_string)
---
+++ function_recursion.py
@@ -0,0 +1,12 @@
+def sum_of_digits(n):
+    if  n< 10:       # Base case: if n is single_digit number, return n 
+        return n  
+
+    else:     #Recursive case: calculate the sum of digits
+        last_digit = n % 10          #Get the last sum of digits
+        remaining_digits = n // 10   #Get the remaining digits by integer division with 10
+
+        return last_digit + sum_of_digits(remaining_digits) #Recursively call the function with the remaining digits
+                                                            #and add the last digit to the result 
+    
+print(sum_of_digits(123))
