abd4b1d73b6482594c333d40ff3add3ee0c24a39
unknown <02230268.cst@rub.edu.bt>
2024-04-03 19:48:53 +0530

    multiplication-exercise

---
+++ task1 _multiplication.py
@@ -0,0 +1,11 @@
+n = int(input("enter the number for your multiplication:"))
+m = int(input("enter the limit for your multiplication:"))
+
+for i in range(1,m+1,):
+    a=n*i
+    print(n,"x",i,"=",a)
+    
+print("You have your required multiplication upto your requested range")
+
+      
+
